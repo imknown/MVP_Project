698caac7145023218264517b16e0f492197961e2
darkwh <543044690@qq.com>
2016-05-30 16:30:37 +0800

    学习中

--- app/src/main/java/com/example/darkwh/mvp_project/MainActivity.java
+++ app/src/main/java/com/example/darkwh/mvp_project/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.darkwh.mvp_project;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 }
